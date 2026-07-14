8d220b08eafea76f3eb07c067d4b20b3fafbf8c6
Michael Wehle <michael.wehle@mt-ag.com>
2026-07-14 15:28:36 +0200

    Un-“naked gun”-nify: implement PEP 8

--- hpo/src/hpo/evaluation/rendering/solar_system_lander/__init__.py
+++ hpo/src/hpo/evaluation/rendering/solar_system_lander/__init__.py
@@ -1,5 +1,5 @@
 """SolarSystemLander rendering API."""
 
-from hpo.evaluation.rendering.solar_system_lander._config import RenderConfig, render_config, wrap_env
+from hpo.evaluation.rendering.solar_system_lander.config import RenderConfig, render_config, wrap_env
 
 __all__ = ["RenderConfig", "render_config", "wrap_env"]
