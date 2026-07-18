a0988a485985e1ccad1f436b5cb548630929cf9f
Michael Wehle <michael.wehle@mt-ag.com>
2026-07-18 18:21:34 +0200

    Add skin colored_eagle_2

--- hpo/src/hpo/evaluation/rendering/solar_system_lander/_skins/__init__.py
+++ hpo/src/hpo/evaluation/rendering/solar_system_lander/_skins/__init__.py
@@ -1,6 +1,7 @@
 """Optional LunarLander visual skins."""
 
 from hpo.evaluation.rendering.solar_system_lander._skins.colored_eagle import ColoredEagleSkin
+from hpo.evaluation.rendering.solar_system_lander._skins.colored_eagle_2 import ColoredEagle2Skin
 from hpo.evaluation.rendering.solar_system_lander._skins.eagle import DetailedEagleSkin
 
-__all__ = ["ColoredEagleSkin", "DetailedEagleSkin"]
+__all__ = ["ColoredEagleSkin", "ColoredEagle2Skin", "DetailedEagleSkin"]
